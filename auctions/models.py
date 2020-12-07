200a2df128dbba6241ada3e24abbdafb7128bcf0
Ilhan Karadeniz <ilhany.karadeniz@gmail.com>
2020-12-08 02:05:38 +0600

    committing everything before setting image to null

--- auctions/models.py
+++ auctions/models.py
@@ -10,7 +10,7 @@ class Auctions(models.Model):
     description = models.TextField()
     first_date = models.DateTimeField(auto_now=True)
     starting_price = models.IntegerField()
-    image = models.ImageField(upload_to='uploads/')
+    image = models.ImageField(upload_to='uploads/',null=True,blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)  
 
     def __str__(self):
